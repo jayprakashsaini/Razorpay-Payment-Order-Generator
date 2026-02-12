571be6643a89d2fe80560351069a4d9b6ad7722c
jayprakashsaini <86846570+jayprakashsaini@users.noreply.github.com>
2026-02-12 18:08:41 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
+import razorpay
+import json
+razorPayId = "rzp_test_SFArDx9biTbx53"
+razorPaySecret = "tROhlCkm9y2tvTORUyxrbf25"
+client = razorpay.Client(auth=(razorPayId, razorPaySecret))
+
 def main(context):
   context.log("Hello from appwrite")
+  params = json.loads(context.req.body)
+  amount = float(params["amount"])*100
+  DATA = {
+      "amount": amount,
+      "currency": "INR",
+      "receipt": "receipt#1",
+      "notes": {
+          "key1": "value3",
+          "key2": "value2"
+      }
+  }
+  response = client.order.create(data=DATA)
+  context.log(f"response is {response}")
